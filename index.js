44f6b173dd2aa73f4a3592442e1898004ee9aeaa
atoor0807 <atoor0807@gmail.com>
2026-06-04 05:05:21 +0500

    Integrate Socket IO

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
 
 const app = express();
 const server = createServer(app);
 
-console.log(app)
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+  console.log('a user disconnected');
+})
+});
+
+
+let __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'))
 });
 
 server.listen(3000, () => {

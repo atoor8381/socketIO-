2f1067e3f66df452833242a3459202392ff93cad
atoor0807 <atoor0807@gmail.com>
2026-06-24 23:33:20 +0500

    Server Delivery

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+// here we are using await bcz the open returns a promise and we need to wait for it to resolve before we can use the db object.
+const db = await open({
+  filename: 'chat.db',
+  driver: sqlite3.Database
+})
+
+await db.exec(
+  `CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );`
+)
 
 
 const app = express();
@@ -12,17 +28,33 @@ const io = new Server(server, {
   connectionStateRecovery: {}
 });
 
-io.on('connection', (socket) => {
+io.on('connection',async (socket) => {
   console.log('a user connected');
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg)
-    console.log('message ' + msg)
+  if (!socket.recovered) {
+    try {
+      await db.each('SELECT id, content FROM messages WHERE id>?', [socket.handshake.auth.socketOffset || 0], (_err, row) => {
+        socket.emit("chat message", row.content, row.id)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.run(`INSERT INTO messages(content) VALUES(?)`, msg)
+    }
+    catch (err) {
+      console.log(err)
+    }
+    io.emit("chat message", msg, result.lastID)
+    console.log('message ' + msg, result.lastID)
   })
-  socket.on("user typing",()=>{
+  socket.on("user typing", () => {
     console.log("user is typing ")
   })
   socket.on('disconnect', () => {
-  console.log('a user disconnected');
+    console.log('a user disconnected');
   })
 });
 

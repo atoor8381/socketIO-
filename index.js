819c980c98a3760a9aec6b99fdc57fb0179731fa
atoor0807 <atoor0807@gmail.com>
2026-06-27 02:24:08 +0500

    Scales horizontally All done

--- index.js
+++ index.js
@@ -5,73 +5,92 @@ import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 
-// here we are using await bcz the open returns a promise and we need to wait for it to resolve before we can use the db object.
-const db = await open({
-  filename: 'chat.db',
-  driver: sqlite3.Database
-})
 
-await db.exec(
-  `CREATE TABLE IF NOT EXISTS messages (
+if(cluster.isPrimary){
+   const numCPUs = availableParallelism();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i
+    });
+  }
+
+  setupPrimary();
+}
+else {
+  // here we are using await bcz the open returns a promise and we need to wait for it to resolve before we can use the db object.
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  })
+
+  await db.exec(
+    `CREATE TABLE IF NOT EXISTS messages (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       client_offset TEXT UNIQUE,
       content TEXT
   );`
-)
+  )
 
 
-const app = express();
-const server = createServer(app);
+  const app = express();
+  const server = createServer(app);
 
-const io = new Server(server, {
-  connectionStateRecovery: {}
-});
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    adapter: createAdapter()
 
-io.on('connection',async (socket) => {
-  console.log('a user connected');
-  if (!socket.recovered) {
-    try {
-      await db.each('SELECT id, content FROM messages WHERE id>?', [socket.handshake.auth.socketOffset || 0], (_err, row) => {
-        socket.emit("chat message", row.content, row.id)
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  socket.on("chat message", async (msg, callback, clientoffset) => {
-    let result;
-    try {
-      result = await db.run(`INSERT INTO messages(content, client_offset) VALUES(?, ?)`, msg, clientoffset)
-    }
-    catch (err) {
-      if(err.errno=19){
-        callback()
+  });
+
+  io.on('connection', async (socket) => {
+    console.log('a user connected');
+    if (!socket.recovered) {
+      try {
+        await db.each('SELECT id, content FROM messages WHERE id>?', [socket.handshake.auth.socketOffset || 0], (_err, row) => {
+          socket.emit("chat message", row.content, row.id)
+        })
+      } catch (error) {
+        console.log(error)
       }
-      return;
     }
-    io.emit("chat message", msg, result.lastID)
-    console.log('message ' + msg, result.lastID)
-  callback()
-  })
-  socket.on("user typing", () => {
-    console.log("user is typing ")
-  })
-  socket.on('disconnect', () => {
-    console.log('a user disconnected');
-  })
-});
+    socket.on("chat message", async (msg, callback, clientoffset) => {
+      let result;
+      try {
+        result = await db.run(`INSERT INTO messages(content, client_offset) VALUES(?, ?)`, msg, clientoffset)
+      }
+      catch (err) {
+        if (err.errno = 19) {
+          callback()
+        }
+        return;
+      }
+      io.emit("chat message", msg, result.lastID)
+      console.log('message ' + msg, result.lastID)
+      callback()
+    })
+    socket.on("user typing", () => {
+      console.log("user is typing ")
+    })
+    socket.on('disconnect', () => {
+      console.log('a user disconnected');
+    })
+  });
 
 
-let __dirname = dirname(fileURLToPath(import.meta.url))
+  let __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'))
-});
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
+  });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+  const port = process.env.PORT;
 
+  server.listen(port, () => {
+    console.log('server running at http://localhost:' + port);
+  });
+}
 // Express creates an HTTP server but doesn't give us enough control to use Socket.io. Thast's why we use both http and the express. 
 
